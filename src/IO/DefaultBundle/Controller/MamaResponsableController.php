3bb5460911bc5944e1e3e57d6986184a8e061ec0
IronPenguin <darksteelpenguin@gmail.com>
2014-09-08 21:04:08 +0200

    First day commit

--- src/IO/DefaultBundle/Controller/MamaResponsableController.php
+++ src/IO/DefaultBundle/Controller/MamaResponsableController.php
@@ -6,22 +6,28 @@ use IO\DefaultBundle\Controller\DefaultController as BaseController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class MamaResponsableController extends BaseController
-{
+class MamaResponsableController extends BaseController {
+
     /**
      * @Route("/mama", name="mama_responsable")
      * @Template()
      */
-    
-    
-    public function indexAction()
-    {
+    public function indexAction() {
         $list = array(
-            array('id' => 1, 'title' => "Premier article", 'date' => new \DateTime()),
-            array('id' => 2, 'title' => "Deuxième article", 'date' => new \DateTime()),
-            array('id' => 3, 'title' => "Troisième article", 'date' => new \DateTime())
+            array('id' => 1, 'title' => "LE TITRE",
+                'short' => "Principe de présentation de photo",
+                'img' => "bundles/iodefault/images/vignette-1.jpg",
+                'content' => "Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un peintre anonyme assembla ensemble des morceaux de texte pour réaliser un livre spécimen de polices de texte. Il n'a pas fait que survivre cinq siècles, mais s'est aussi adapté à la bureautique informatique, sans que son contenu n'en soit modifié. Il a été popularisé dans les années 1960 grâce à la vente de feuilles Letraset contenant des passages du Lorem Ipsum, et, plus récemment, par son inclusion dans des applications de mise en page de texte, comme Aldus PageMaker."),
+            array('id' => 2, 'title' => "LE TITRE",
+                'short' => "Principe de présentation de photo",
+                'img' => "bundles/iodefault/images/vignette-1.jpg",
+                'content' => "Contrairement à une opinion répandue, le Lorem Ipsum n'est pas simplement du texte aléatoire. Il trouve ses racines dans une oeuvre de la littérature latine classique datant de 45 av. J.-C., le rendant vieux de 2000 ans. Un professeur du Hampden-Sydney College, en Virginie, s'est intéressé à un des mots latins les plus obscurs, consectetur, extrait d'un passage du Lorem Ipsum, et en étudiant tous les usages de ce mot dans la littérature classique, découvrit la source incontestable du Lorem Ipsum. Il provient en fait des sections 1.10.32 et 1.10.33 du De Finibus Bonorum et Malorum (Des Suprêmes Biens et des Suprêmes Maux) de Cicéron. Cet ouvrage, très populaire pendant la Renaissance, est un traité sur la théorie de l'éthique. Les premières lignes du Lorem Ipsum, Lorem ipsum dolor sit amet..., proviennent de la section 1.10.32."),
+            array('id' => 3, 'title' => "LE TITRE",
+                'short' => "Principe de présentation de photo",
+                'img' => "bundles/iodefault/images/vignette-1.jpg",
+                'content' => "On sait depuis longtemps que travailler avec du texte lisible et contenant du sens est source de distractions, et empêche de se concentrer sur la mise en page elle-même. L'avantage du Lorem Ipsum sur un texte générique comme 'Du texte. Du texte. Du texte.' est qu'il possède une distribution de lettres plus ou moins normale, et en tout cas comparable avec celle du français standard. De nombreuses suites logicielles de mise en page ou éditeurs de sites Web ont fait du Lorem Ipsum leur faux texte par défaut, et une recherche pour 'Lorem Ipsum' vous conduira vers de nombreux sites qui n'en sont encore qu'à leur phase de construction. Plusieurs versions sont apparues avec le temps, parfois par accident, souvent intentionnellement (histoire d'y rajouter de petits clins d'oeil, voire des phrases embarassantes).")
         );
-        return $this->render('IODefaultBundle:MamaResponsable:index.html.twig',
-                array('articles' => $list));
+        return $this->render('IODefaultBundle:MamaResponsable:index.html.twig', array('articles' => $list));
     }
+
 }
