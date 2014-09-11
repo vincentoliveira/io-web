4f705618c6bded9bd40ed94a72b05218c7011346
vincentoliveira <vincent@oliveira.fr>
2014-09-11 11:59:33 +0200

    Fixed GoogleMap

--- src/IO/DefaultBundle/Controller/TrouverMamaController.php
+++ src/IO/DefaultBundle/Controller/TrouverMamaController.php
@@ -5,7 +5,6 @@ namespace IO\DefaultBundle\Controller;
 use IO\DefaultBundle\Controller\DefaultController as BaseController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
-use IO\DefaultBundle\Utils\GoogleMapAPI;
 
 
 class TrouverMamaController extends BaseController {
@@ -16,18 +15,7 @@ class TrouverMamaController extends BaseController {
      */
     public function indexAction()
     {
-        $map = new GoogleMapAPI('map');
-        $map->setAPIKey('AIzaSyAv4BIv4XIqzUNvG-Pcnsvn282QuyxefuM');
-        
-        $map->setWidth('800px');
-        $map->setHeight('500px');
-        $map->setCenterCoords('2.352469', '48.867142');
-        $map->setZoomLevel(5);
-        $map->setControlSize('small');
-        
-        //$list = array($map);
-        return $this->render('IODefaultBundle:TrouverMama:index.html.twig', array('googlemap' => $map->printMap()));
-        //return array();
+        return array();
     }
 
 }
