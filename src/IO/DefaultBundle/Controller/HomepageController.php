5750058c04291eb67ef8217dd9bd4bb6de04c995
vincentoliveira <vincent@oliveira.fr>
2014-09-15 14:56:44 +0200

    Fix homepage

--- src/IO/DefaultBundle/Controller/HomepageController.php
+++ src/IO/DefaultBundle/Controller/HomepageController.php
@@ -2,14 +2,14 @@
 
 namespace IO\DefaultBundle\Controller;
 
-use IO\DefaultBundle\Controller\DefaultController as BaseController;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 
-class HomeController extends BaseController
+class HomepageController extends DefaultController
 {
+
     /**
-     * @Route("/home", name="home_site")
+     * @Route("/", name="homepage")
      * @Template()
      */
     public function indexAction()
